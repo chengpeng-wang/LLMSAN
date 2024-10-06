7ac69b9cfc841ce6a1d19a8462a408225e927718
chengpeng-wang <15705513022@163.com>
2024-10-06 17:22:46 -0400

    update the tree-sitter version

--- lib/build.py
+++ lib/build.py
@@ -10,6 +10,9 @@ if not (cwd / "vendor/tree-sitter-java/grammar.js").exists():
     os.system(
         f'git clone https://github.com/tree-sitter/tree-sitter-java.git {cwd / "vendor/tree-sitter-java"}'
     )
+    os.system(
+        f'cd {cwd / "vendor/tree-sitter-java"} && git checkout 490d878cf33b0ad5ae7a7253ff30597a5bdc348e && cd {cwd}'
+    )
 
 Language.build_library(
     # Store the library in the `build` directory
